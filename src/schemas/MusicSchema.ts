e60feb71391d3ad4cdd352d1f47c769ffa09e1e5
Chacaponquin <hectorangel2001@gmail.com>
2022-10-02 22:44:51 -0400

    Add SECURITY.md

--- src/schemas/MusicSchema.ts
+++ src/schemas/MusicSchema.ts
@@ -1,12 +1,12 @@
-import { faker } from "@faker-js/faker";
-import { SchemaField } from "../utils/SchemaField";
+import { faker } from '@faker-js/faker';
+import { SchemaField } from '../utils/SchemaField';
 
 export class MusicSchema {
   public songName(): SchemaField {
-    return new SchemaField("songName", faker.music.songName, {});
+    return new SchemaField<string>('songName', faker.music.songName, {});
   }
 
   public gender(): SchemaField {
-    return new SchemaField("gender", faker.music.genre, {});
+    return new SchemaField<string>('gender', faker.music.genre, {});
   }
 }
