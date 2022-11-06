ce014aaac55c1b192461916cfe91244a82165ca3
Chacaponquin <hectorangel2001@gmail.com>
2022-11-06 16:44:41 -0500

    Add Capitaliza tests

--- test/library/util.test.ts
+++ test/library/util.test.ts
@@ -1,5 +1,21 @@
 import mocha from "mocha";
 import { expect } from "chai";
 import { PrivateUtils } from "../../src/utils/helpers/PrivateUtils";
+import { CHDataError } from "../../src/errors/CHDataError";
 
-describe("#Util Tests", () => {});
+describe("#Util Tests", () => {
+  context("capitalizaText test", () => {
+    it("passing undefined as argument. Should return an error", () => {
+      try {
+        const value = PrivateUtils.capitalizeText(undefined!);
+      } catch (error) {
+        expect(error).to.be.instanceOf(CHDataError);
+      }
+    });
+
+    it("passing 'Hello World' as argument. Should return 'helloWorld'", () => {
+      const value = PrivateUtils.capitalizeText("Hello World");
+      expect(value).to.be.equal("helloWorld");
+    });
+  });
+});
--- test/schemas/dataType.test.ts
+++ test/schemas/dataType.test.ts
@@ -1,21 +1,21 @@
 import mocha from "mocha";
 import { expect } from "chai";
-import { PrivateUtils } from "../../src/utils/helpers/PrivateUtils";
+import { chaca, schemas } from "../../src";
 
 describe("#DataType Tests", () => {
   context("intNumber test", () => {
     it("Passing a max value (5). Should return a int number less than 5", () => {
-      const value = PrivateUtils.intNumber({ max: 5 });
+      const value = schemas.dataType.int().getValue({ max: 5 });
       expect(value <= 5 && Number.isInteger(value)).to.be.true;
     });
 
     it("Passing min: 0 && max: 100. Should return an int number between 0 and 100", () => {
-      const value = PrivateUtils.intNumber({ max: 100, min: 0 });
+      const value = schemas.dataType.int().getValue({ max: 100, min: 0 });
       expect(value <= 100 && value >= 0 && Number.isInteger(value)).to.be.true;
     });
 
     it("Passing min: -10 && max: -1. Should return an int number between -1 and -10", () => {
-      const value = PrivateUtils.intNumber({ max: -1, min: -10 });
+      const value = schemas.dataType.int().getValue({ max: -1, min: -10 });
       expect(value <= -1 && value >= -10 && Number.isInteger(value)).to.be.true;
     });
   });
