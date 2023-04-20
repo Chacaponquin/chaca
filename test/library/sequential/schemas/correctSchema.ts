a6dff2163fa52e7565f778bfb0eaedabe32d1d49
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-04-20 13:33:34 -0400

    Add first sequential test

---
+++ test/library/sequential/schemas/correctSchema.ts
@@ -0,0 +1,9 @@
+import { chaca, schemas } from "../../../../src";
+
+export const CORRECT_SEQUENTIAL_SCHEMA = chaca.defineSchema({
+  id: schemas.id.uuid(),
+  name: schemas.person.fullName(),
+  favoriteNumber: chaca.sequential([1, 2, 3, 4]),
+});
+
+export const CORRECT_SEQUENTIAL_DATA = CORRECT_SEQUENTIAL_SCHEMA.generate(4);
---
+++ test/library/sequential/sequential.test.ts
@@ -0,0 +1,16 @@
+import { CORRECT_SEQUENTIAL_DATA } from "./schemas/correctSchema";
+import { FEW_VALUES_SEQUENTIAL_SCHEMA } from "./schemas/incorrectSchemas";
+
+describe("# Sequential Field test", () => {
+  it("Correct define of a schema with sequential field", () => {
+    expect(CORRECT_SEQUENTIAL_DATA.length).toBe(4);
+    expect(CORRECT_SEQUENTIAL_DATA[0].favoriteNumber).toBe(1);
+    expect(CORRECT_SEQUENTIAL_DATA[1].favoriteNumber).toBe(2);
+    expect(CORRECT_SEQUENTIAL_DATA[2].favoriteNumber).toBe(3);
+    expect(CORRECT_SEQUENTIAL_DATA[3].favoriteNumber).toBe(4);
+  });
+
+  it("Not enought values for the generate data. Should return an error", () => {
+    expect(FEW_VALUES_SEQUENTIAL_SCHEMA.generate(10)).toThrow();
+  });
+});
