a39d3ce3fb7c9be914b76b0ab8a9589d7652a16f
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-04-20 14:08:44 -0400

    Add more sequential test

--- test/library/sequential/schemas/incorrectSchemas.ts
+++ test/library/sequential/schemas/incorrectSchemas.ts
@@ -6,17 +6,19 @@ export const EMPTY_SEQUENTIAL_SCHEMA = chaca.defineSchema({
   favoriteNumber: chaca.sequential([]),
 });
 
-export const STRING_SEQUENTIAL_SCHEMA = chaca.defineSchema({
-  id: schemas.id.uuid(),
-  name: schemas.person.fullName(),
-  favoriteNumber: "" as any,
-});
+export const STRING_SEQUENTIAL_SCHEMA = () =>
+  chaca.defineSchema({
+    id: schemas.id.uuid(),
+    name: schemas.person.fullName(),
+    favoriteNumber: chaca.sequential("" as any),
+  });
 
-export const NUMBER_SEQUENTIAL_SCHEMA = chaca.defineSchema({
-  id: schemas.id.uuid(),
-  name: schemas.person.fullName(),
-  favoriteNumber: 5 as any,
-});
+export const NUMBER_SEQUENTIAL_SCHEMA = () =>
+  chaca.defineSchema({
+    id: schemas.id.uuid(),
+    name: schemas.person.fullName(),
+    favoriteNumber: chaca.sequential(5 as any),
+  });
 
 export const FEW_VALUES_SEQUENTIAL_SCHEMA = chaca.defineSchema({
   id: schemas.id.uuid(),
