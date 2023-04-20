fae93bcec0464c78d8aa5815ddb17c176834adea
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-04-20 17:28:50 -0400

    Update and refactoring the MultiGeneratorResolver

--- src/utils/helpers/MultiGenerate/classes/MultiGenerateResolver.ts
+++ src/utils/helpers/MultiGenerate/classes/MultiGenerateResolver.ts
@@ -6,7 +6,15 @@ export class MultiGenerateResolver<K> {
   private resolversArray: Array<SchemaResolver>;
 
   constructor(schemas: Array<MultiGenerateSchema>) {
-    this.resolversArray = schemas.map((s) => {
+    this.resolversArray = this.createSchemaResolvers(schemas);
+    this.injectSchemas();
+    this.buildInputTrees();
+  }
+
+  private createSchemaResolvers(
+    schemas: Array<MultiGenerateSchema>,
+  ): Array<SchemaResolver> {
+    return schemas.map((s) => {
       if (typeof s === "object" && s !== null) {
         return new SchemaResolver(
           s.name,
@@ -17,13 +25,17 @@ export class MultiGenerateResolver<K> {
         throw new ChacaError("You must provide a name to your schema");
       }
     });
+  }
 
+  private injectSchemas(): void {
     // inyectar los schema resolvers restantes a cada schema resolver
     for (const resolver of this.resolversArray) {
       const otherResolvers = this.resolversArray.filter((r) => r !== resolver);
       resolver.setInjectedSchemas(otherResolvers);
     }
+  }
 
+  private buildInputTrees(): void {
     this.resolversArray.forEach((r) => r.buildInputTree());
   }
 
