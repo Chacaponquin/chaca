f96f9b141d8e4795f89e1c7232defd34558441b4
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-06-13 21:14:11 -0400

    Fix Javascript generator add symbols and function export

--- src/generators/Javascript/JavascriptGenerator.ts
+++ src/generators/Javascript/JavascriptGenerator.ts
@@ -2,6 +2,7 @@ import { FileConfig } from "../../core/interfaces/export.interface.js";
 import { Generator } from "../Generator.js";
 import fs from "fs";
 import { PrivateUtils } from "../../core/helpers/PrivateUtils.js";
+import { ChacaError } from "../../errors/ChacaError.js";
 
 export class JavascriptGenerator extends Generator {
   constructor(data: any, config: FileConfig) {
@@ -51,6 +52,16 @@ export class JavascriptGenerator extends Generator {
       returnValue = `${JSON.stringify(value)}`;
     } else if (typeof value === "number" || typeof value === "boolean") {
       returnValue = `${value}`;
+    } else if (typeof value === "undefined") {
+      returnValue = "undefined";
+    } else if (typeof value === "bigint") {
+      returnValue = `${value.toString()}n`;
+    } else if (typeof value === "function") {
+      throw new ChacaError(
+        `You can not export a function to a javascript file.`,
+      );
+    } else if (typeof value === "symbol") {
+      throw new ChacaError(`You can not export a Symbol to a javascript file.`);
     } else if (typeof value === "object") {
       if (Array.isArray(value)) {
         returnValue = this.generateArray(value);
