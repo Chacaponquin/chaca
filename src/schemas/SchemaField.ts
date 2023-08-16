4446b3c59d7a3164fd9fbfb9969974afb8c8f1df
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-08-16 14:22:17 -0400

    Refactor args in schema field

--- src/schemas/SchemaField.ts
+++ src/schemas/SchemaField.ts
@@ -2,19 +2,33 @@ interface ISchemaField<T, K> {
   getValue(args: T): K;
 }
 
+class Args<A> {
+  private _value: A | null = null;
+
+  constructor(args?: A) {
+    if (args && typeof args === "object" && !Array.isArray(args)) {
+      this._value = args;
+    }
+  }
+
+  public value() {
+    return this._value;
+  }
+}
+
 export class SchemaField<K = any, A = any> implements ISchemaField<A, K> {
   private valueFunction: (args: A) => K;
   private args: A;
 
   constructor(func: (args: A) => K, args?: A) {
     this.valueFunction = func;
-    this.args = args || ({} as A);
+    this.args = new Args(args).value() || ({} as A);
   }
 
   public getValue(a?: A): K {
-    const args =
-      a && typeof a === "object" && !Array.isArray(a) ? a : this.args;
+    const newArgs = new Args(a).value();
+    const value = this.valueFunction(newArgs === null ? this.args : newArgs);
 
-    return this.valueFunction(args);
+    return value;
   }
 }
