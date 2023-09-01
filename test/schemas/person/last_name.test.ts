6348332862c66b01bbd8c986e603aa7fc0704333
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-09-01 13:24:36 -0400

    Add person last name tests

--- test/schemas/person/last_name.test.ts
+++ test/schemas/person/last_name.test.ts
@@ -1,1 +1,24 @@
-describe("# Person last name tests", () => {});
+import { schemas } from "../../../src";
+
+describe("# Person last name tests", () => {
+  it("Pass no arguments. Should return one an english lastnames", () => {
+    const value = schemas.person.lastName().getValue();
+    const is = schemas.person.constants.names.en.lastNames.includes(value);
+
+    expect(is).toBe(true);
+  });
+
+  it("Pass a not valid language argument. Should return an english lastnames", () => {
+    const value = schemas.person.lastName({ language: "" as any }).getValue();
+    const is = schemas.person.constants.names.en.lastNames.includes(value);
+
+    expect(is).toBe(true);
+  });
+
+  it("Pass language=es. Should return one of the defined lastnames", () => {
+    const value = schemas.person.lastName({ language: "es" }).getValue();
+    const is = schemas.person.constants.names.es.lastNames.includes(value);
+
+    expect(is).toBe(true);
+  });
+});
