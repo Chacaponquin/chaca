543eeef26e17835ea2addaa8b3c8f2dda02c4b7e
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-10-24 14:34:31 -0400

    fix: fix core/export name

--- src/core/Export/generators/json/core/creator.ts
+++
@@ -1,9 +0,0 @@
-import { SpaceIndex } from "../../../core/space-index";
-
-export class JsonCodeCreator {
-  constructor(private readonly indent: SpaceIndex) {}
-
-  execute(data: any): string {
-    return JSON.stringify(data, undefined, this.indent.step());
-  }
-}
