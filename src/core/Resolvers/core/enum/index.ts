21a6e0834a64e0e501154cc9728ada9b19d0f817
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-10-24 14:43:34 -0400

    refactor: change resolvers name

--- src/core/Resolvers/core/enum/index.ts
+++
@@ -1,7 +0,0 @@
-import { IResolver } from "../../interfaces/resolvers";
-
-export class EnumFieldResolver<R> extends IResolver {
-  constructor(readonly array: ReadonlyArray<R>) {
-    super();
-  }
-}
