de78f933a75745e975697e6f055ffc9568beab2f
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-11-02 01:13:38 -0400

    test: add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: { testTimeout: 100000 },
+  test: {
+    testTimeout: 100000,
+    coverage: {
+      provider: "istanbul",
+      reporter: ["json", "html"],
+    },
+  },
 });
