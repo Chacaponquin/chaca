e62a088c01b7852ac03b8fe7ace2fe259fdbd5bf
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-11-04 10:47:08 -0500

    fix: fix schema cli tests

--- test/cli/schema/schema.test.ts
+++ test/cli/schema/schema.test.ts
@@ -4,7 +4,7 @@ import { describe, expect, it } from "vitest";
 describe("Schema CLI", () => {
   it("export 50 objects from schema", () => {
     const result = exec(
-      "node ./lib/bin/index.js json --count 50 --filename schema-array --output data/cli/schema --config test/cli/dataset/schema.js",
+      "node ./lib/bin/index.js json --count 50 --filename schema-array --output data/cli/schema --config test/cli/schema/schema.js",
     );
 
     expect(result.code).toBe(0);
