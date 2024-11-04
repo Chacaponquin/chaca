3a259483964a3781a5a23356ad41acea75050303
Héctor Angel Gomez <hectorangel2001@gmail.com>
2024-11-04 10:40:15 -0500

    test: add empty schema name tests

--- test/library/dataset/dataset.test.ts
+++ test/library/dataset/dataset.test.ts
@@ -27,4 +27,32 @@ describe("Dataset", () => {
 
     expect(() => dataset.generate()).toThrow(ChacaError);
   });
+
+  describe("schemas without name", () => {
+    it("trying export a schema with name = ''. should throw an error", () => {
+      const schema = chaca.schema({});
+
+      const schema2 = chaca.schema({});
+
+      const dataset = chaca.dataset([
+        { name: "", documents: 10, schema: schema },
+        { name: "schema", documents: 10, schema: schema2 },
+      ]);
+
+      expect(() => dataset.generate()).toThrow(ChacaError);
+    });
+
+    it("trying export a schema with name = '   '. should throw an error", () => {
+      const schema = chaca.schema({});
+
+      const schema2 = chaca.schema({});
+
+      const dataset = chaca.dataset([
+        { name: "     ", documents: 10, schema: schema },
+        { name: "schema", documents: 10, schema: schema2 },
+      ]);
+
+      expect(() => dataset.generate()).toThrow(ChacaError);
+    });
+  });
 });
